14a8d065a137a88ab171762e28e534a3e1fa1807
David <dad9489@rit.edu>
2022-12-07 19:33:33 -0700

    litter robot integration with api

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 from flask import Flask
-from dotenv import load_dotenv
-import os
+from apscheduler.schedulers.background import BackgroundScheduler
+from litter_robot import WHISKER_ACCOUNT, scheduled_refresh, trigger_cleaning, get_info
 
 app = Flask(__name__)
 
-load_dotenv()
-WHISKER_USERNAME = os.getenv('WHISKER_USERNAME')
-WHISKER_PASSWORD = os.getenv('WHISKER_PASSWORD')
+
+@app.route('/trigger_cleaning', methods=['POST'])
+def cleaning_route():
+    return trigger_cleaning()
 
 
-@app.route('/')
-def index():
-    return f"Loaded env, username: {WHISKER_USERNAME}"
+@app.route('/info', methods=['GET'])
+def info_route():
+    return get_info()
+
+
+sched = BackgroundScheduler(daemon=True, timezone='US/Mountain')
+sched.add_job(scheduled_refresh, 'cron', hour='*')
+sched.start()
 
 
 if __name__ == '__main__':
+    print('test')
+    print(WHISKER_ACCOUNT)
     app.run(host='0.0.0.0', port=5000)
--- litter_robot.py
+++ litter_robot.py
@@ -2,31 +2,88 @@ import asyncio
 
 from pylitterbot import Account
 from dotenv import load_dotenv
-
-load_dotenv()
-username = os.getenv('WHISKER_USERNAME')
-password = os.getenv('WHISKER_PASSWORD')
+import os
 
 
-async def main():
-    # Create an account.
+async def get_account():
+    load_dotenv()
+    username = os.getenv('WHISKER_USERNAME')
+    password = os.getenv('WHISKER_PASSWORD')
     account = Account()
+    await account.connect(username=username, password=password, load_robots=True)
+    return account
 
+WHISKER_ACCOUNT = asyncio.run(get_account())
+
+
+def scheduled_refresh():
+    global WHISKER_ACCOUNT
+    print('refreshing account...')
+    new_account = asyncio.run(get_account())
+    WHISKER_ACCOUNT = new_account
+    print('finished refreshing account')
+
+
+def trigger_cleaning():
+    success = asyncio.run(WHISKER_ACCOUNT.robots[0].start_cleaning())
+    return success
+
+
+def do_sync(func):
+    asyncio.get_event_loop().close()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     try:
-        # Connect to the API and load robots.
-        await account.connect(username=username, password=password, load_robots=True)
-
-        # Print robots associated with account.
-        print("Robots:")
-        for robot in account.robots:
-            print(robot)
-
-            test = await robot.start_cleaning()
-            print(test)
+        res = loop.run_until_complete(func())
     finally:
-        # Disconnect from the API.
-        await account.disconnect()
+        loop.close()
+    return res
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+def get_info():
+    robots = [{k: v for k, v in [x.split(': ') for x in str(x).split(', ')]} for x in WHISKER_ACCOUNT.robots]
+    return {
+        'robots': robots,
+        'user': WHISKER_ACCOUNT._user,
+        'insight': asyncio.run(WHISKER_ACCOUNT.robots[0].get_insight())
+    }
+
+
+# async def get_insight():
+#     try:
+#         a = []
+#         for robot in WHISKER_ACCOUNT.robots:
+#             a = await robot.get_insight()
+#     except Exception as e:
+#         if str(e) != 'Event loop is closed':
+#             raise e
+#         else:
+#             return a
+#     finally:
+#         return a
+
+
+# async def main():
+#     try:
+#         account = await get_account()
+#
+#         # Print robots associated with account.
+#         print("Robots:")
+#         for robot in account.robots:
+#             print(robot)
+#
+#             test = await robot.get_insight()
+#             print(test)
+#     finally:
+#         # Disconnect from the API.
+#         await account.disconnect()
+
+
+# asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+
+# if __name__ == "__main__":
+#     try:
+#         print(do_sync(get_insight))
+#     except Exception as e:
+#         x = 1
